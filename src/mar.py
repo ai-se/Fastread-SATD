2671a56082c09fd2dcced6d96e17e23db80357b5
ffahid <ffahid@ncsu.edu>
2018-12-04 14:06:10 -0500

    updated after experimenting

--- src/mar.py
+++ src/mar.py
@@ -1,4 +1,5 @@
 import pickle
+import random
 from pdb import set_trace
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -10,18 +11,26 @@ import time
 import os
 from sklearn import preprocessing
 
+import pandas
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.disabled = True
 
 class MAR(object):
     def __init__(self):
+        self.fea_count = 40       # FAHID feature counts for building TFIDF
         self.step = 10              # FAHID after how many steps we want to train again
         self.enough = 30            # FAHID convert to agressive undersampling
         self.atleast = 100          # FAHID if we have unlabeled data, assume all negative (as the chances are very low)
         self.interval = 500000000   # FAHID should we do error correction or not, or something to do with that
+        random.seed(0)
 
     def create(self, filename):
         self.filename = filename
         self.hasLabel = True
-        self.body = {}
+        self.body = None
         self.round = 0
 
         try:
@@ -32,84 +41,53 @@ class MAR(object):
             print("Data file not found")
         return self
 
-
     def loadfile(self):
-        with open("../workspace/data/" + str(self.filename), "r") as csvfile:
+        with open("../workspace/td/" + self.filename, "r") as csvfile:
             content = [x for x in csv.reader(csvfile, delimiter=',')]
-        fields = ["Document Title", "Abstract", "Year", "PDF Link"]
-        header = content[0]
-        for field in fields:
-            ind = header.index(field)
-            self.body[field] = [c[ind] for c in content[1:]]
-        try:
-            ind = header.index("label")
-            self.body["label"] = [c[ind] for c in content[1:]]
-        except:
-            self.hasLabel = False
-            self.body["label"] = ["unknown"] * (len(content) - 1)
-        try:
-            ind = header.index("code")
-            self.body["code"] = [c[ind] for c in content[1:]]
-        except:
-            self.body["code"] = ['undetermined'] * (len(content) - 1)
-        try:
-            ind = header.index("time")
-            self.body["time"] = [c[ind] for c in content[1:]]
-        except:
-            self.body["time"] = [0] * (len(content) - 1)
-        try:
-            ind = header.index("syn_error")
-            self.body["syn_error"] = [c[ind] for c in content[1:]]
-        except:
-            self.body["syn_error"] = [0] * (len(content) - 1)
-        try:
-            ind = header.index("fixed")
-            self.body["fixed"] = [c[ind] for c in content[1:]]
-        except:
-            self.body["fixed"] = [0] * (len(content) - 1)
-        try:
-            ind = header.index("count")
-            self.body["count"] = [c[ind] for c in content[1:]]
-        except:
-            self.body["count"] = [0] * (len(content) - 1)
+
+        project_based_content = [x for x in content[1:] if x[0]=='sql12']
+
+        data_pd = pandas.DataFrame(project_based_content[1:], columns=content[0])
+
+        data_pd['label'] = np.where((data_pd['classification'] != "WITHOUT_CLASSIFICATION"), 'yes', 'no')
+        data_pd['code'] = 'undetermined'
+        data_pd['time'] = 0
+        data_pd['fixed'] = 0
+        data_pd['count'] = 0
+        true_count = len(data_pd[(data_pd['label'] == 'yes')])
+        false_count = len(data_pd[(data_pd['label'] == 'no')])
+        print("Ground Truth: True " + str(true_count) + " | False " + str(false_count))
+
+
+        self.body = data_pd
         return
 
     # FAHID predicted results
     def get_numbers(self):
         total = len(self.body["code"])
-        pos = Counter(self.body["code"])["yes"]
-        neg = Counter(self.body["code"])["no"]
+        pos = len(self.body[self.body["code"] == 'yes'])
+        neg = len(self.body[self.body["code"] == 'no'])
 
         self.pool = np.where(np.array(self.body['code']) == "undetermined")[0]
         self.labeled = list(set(range(len(self.body['code']))) - set(self.pool))
         return pos, neg, total
 
-
     def preprocess(self):
-        ### Combine title and abstract for training ###########
-        content = [self.body["Document Title"][index] + " " + self.body["Abstract"][index] for index in
-                   range(len(self.body["Document Title"]))]
-        #######################################################
-
         ### Feature selection by tfidf in order to keep vocabulary ###
         tfidfer = TfidfVectorizer(lowercase=True, stop_words="english", norm=None, use_idf=True, smooth_idf=False,
-                                  sublinear_tf=False, decode_error="ignore", max_features=4000)
-        tfidfer.fit(content)
-        self.voc = list(tfidfer.vocabulary_.keys())
+                                  sublinear_tf=False, decode_error="ignore", max_features=self.fea_count)
+        tfidfer.fit(self.body['commenttext'])
 
-        ##############################################################
+        self.voc = list(tfidfer.vocabulary_.keys())
 
         ### Term frequency as feature, L2 normalization ##########
         tfer = TfidfVectorizer(lowercase=True, stop_words="english", norm='l2', use_idf=False,
                                vocabulary=self.voc, decode_error="ignore")
-        # tfer = TfidfVectorizer(lowercase=True, stop_words="english", norm=None, use_idf=False,
-        #                 vocabulary=self.voc,decode_error="ignore")
-        self.csr_mat = tfer.fit_transform(content)
-        ########################################################
+
+        self.csr_mat = tfer.fit_transform(self.body['commenttext'])
         return
 
-
-    ## BM25 ##
+    ## FAHID TODO GOING TO REMOVE ##
     def BM25(self, query):
         if query[0] == '':
             # FAHID This is not a random choice, but to generate n random numbers between [0,1)
@@ -149,49 +127,83 @@ class MAR(object):
     def BM25_get(self):
         # FAHID: get the indexes of bm at indexes of pool, then reverse and take the first step size of them
         return self.pool[np.argsort(self.bm[self.pool])[::-1][:self.step]]
+    ## FAHID TODO END ##
+
+    def get_random_ids(self):
+        return self.body.sample(self.step).index
 
     ## Train model ##
     def train(self, pne=True, weighting=True):
-
         clf = svm.SVC(kernel='linear', probability=True, class_weight='balanced') if weighting else svm.SVC(
             kernel='linear', probability=True)
-        poses = np.where(np.array(self.body['code']) == "yes")[0]
-        negs = np.where(np.array(self.body['code']) == "no")[0]
-        left = poses
-        decayed = list(left) + list(negs)
-        unlabeled = np.where(np.array(self.body['code']) == "undetermined")[0]
+
+        poses = self.body.loc[self.body['code'] == 'yes']
+        negs = self.body.loc[self.body['code'] == 'no']
+        labeled = self.body.loc[self.body['code'] != 'undetermined']
+        unlabeled = self.body.loc[self.body['code'] == 'undetermined']
+
+        pos_ids = list(poses.index)
+        labeled_ids = list(labeled.index)
+
         try:
-            unlabeled = np.random.choice(unlabeled, size=np.max((len(left), self.atleast)), replace=False)
+            unlabeled = unlabeled.sample(self.atleast)
         except:
             pass
 
+        # TODO FAHID PRESUMTIVE NON RELEVANT AFTER APPLYING BM25
+        # Examples Presume all examples are false, because true examples are few
+        # This reduces the biasness of not doing random sampling
         if not pne:
             unlabeled = []
 
-        labels = np.array([x if x != 'undetermined' else 'no' for x in self.body['code']])
-        all_neg = list(negs) + list(unlabeled)
-        sample = list(decayed) + list(unlabeled)
+        unlabeled_ids = unlabeled.index
 
-        clf.fit(self.csr_mat[sample], labels[sample])
+        labels = np.array([x if x != 'undetermined' else 'no' for x in self.body['code']])
+        all_neg = pandas.concat([negs, unlabeled])
+        all_neg_ids = list(all_neg.index)
+
+        sample = pandas.concat([poses, negs, unlabeled])
+        sample_ids = list(sample.index)
+
+        start = time.time()
+
+        clf.fit(self.csr_mat[sample_ids], labels[sample_ids])
+
+        logger.info("after fitting: " + str(time.time() - start))
 
         ## aggressive undersampling ##
         if len(poses) >= self.enough:
+            start = time.time()
 
-            train_dist = clf.decision_function(self.csr_mat[all_neg])
+            train_dist = clf.decision_function(self.csr_mat[all_neg_ids])
             pos_at = list(clf.classes_).index("yes")
             if pos_at:
                 train_dist = -train_dist
-            negs_sel = np.argsort(train_dist)[::-1][:len(left)]
-            sample = list(left) + list(np.array(all_neg)[negs_sel])
-            clf.fit(self.csr_mat[sample], labels[sample])
+            negs_sel = np.argsort(train_dist)[::-1][:len(pos_ids)]
+            sample_ids = list(pos_ids) + list(np.array(all_neg_ids)[negs_sel])
+
+            logger.info("before fitting > enough: " + str(time.time() - start))
+            start = time.time()
+
+            clf.fit(self.csr_mat[sample_ids], labels[sample_ids])
+
+            logger.info("after fitting > enough: " + str(time.time() - start))
         elif pne:
-            train_dist = clf.decision_function(self.csr_mat[unlabeled])
+            start = time.time()
+
+            train_dist = clf.decision_function(self.csr_mat[unlabeled_ids])
             pos_at = list(clf.classes_).index("yes")
             if pos_at:
                 train_dist = -train_dist
-            unlabel_sel = np.argsort(train_dist)[::-1][:int(len(unlabeled) / 2)]
-            sample = list(decayed) + list(np.array(unlabeled)[unlabel_sel])
-            clf.fit(self.csr_mat[sample], labels[sample])
+            unlabel_sel = np.argsort(train_dist)[::-1][:int(len(unlabeled_ids) / 2)]
+            sample_ids = list(labeled_ids) + list(np.array(unlabeled_ids)[unlabel_sel])
+
+            logger.info("before fitting pne: " + str(time.time() - start))
+            start = time.time()
+
+            clf.fit(self.csr_mat[sample_ids], labels[sample_ids])
+
+            logger.info("after fitting pne: " + str(time.time() - start))
 
         uncertain_id, uncertain_prob = self.uncertain(clf)
         certain_id, certain_prob = self.certain(clf)
@@ -227,20 +239,17 @@ class MAR(object):
 
     ## Code candidate studies ##
     def code(self, id, label):
-        self.body["code"][id] = label
-        self.body["time"][id] = time.time()
+        self.body.loc[id, 'code'] = label
+        self.body.loc[id, 'time'] = time.time()
 
     def code_error(self, id, error='none'):
         # FAHID: simulate a human reader
-        if error == 'circle':
-            self.code_circle(id, self.body['label'][id])
-        elif error == 'random':
-            self.code_random(id, self.body['label'][id])
+        if error == 'random':
+            self.code_random(id, self.body.loc[id, 'label'])
         elif error == 'three':
             self.code_three(id, self.body['label'][id])
         else:
-            self.code(id, self.body['label'][id])
-
+            self.code(id, self.body.loc[id, 'label'])
 
     def code_three(self, id, label):
         self.code_random(id, label)
@@ -248,9 +257,7 @@ class MAR(object):
         if self.body['fixed'][id] == 0:
             self.code_random(id, label)
 
-
     def code_random(self, id, label):
-        import random
         error_rate = 0.3
         if label == 'yes':
             if random.random() < error_rate:
@@ -262,13 +269,14 @@ class MAR(object):
                 new = 'yes'
             else:
                 new = 'no'
-        if new == self.body["code"][id]:
-            self.body['fixed'][id] = 1
-        self.body["code"][id] = new
-        self.body["time"][id] = time.time()
-        self.body["count"][id] = self.body["count"][id] + 1
+        if new == self.body.loc[id, "code"]:
+            self.body.loc[id, 'fixed'] = 1
+        self.body.loc[id, "code"] = new
+        self.body.loc[id, "time"] = time.time()
+        self.body.loc[id, "count"] = self.body.loc[id, "count"] + 1
 
 
     def get_allpos(self):
-        return len([1 for c in self.body["label"] if c == "yes"])
+        return len(self.body[self.body['label'] == 'yes'])
+
 
--- src/runner.py
+++ src/runner.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function
 import numpy as np
 from pdb import set_trace
+
 from demos import cmd
 import pickle
 import matplotlib.pyplot as plt
@@ -9,32 +10,39 @@ from sk import rdivDemo
 import random
 from collections import Counter
 
+import pandas
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 
-def active_learning(filename, query='', stopat=.95, error='none', interval=100000, seed=0):
+# FAHID NOTE I dont think we need to change anything here for the new dataset
+def active_learning(filename, query='', stopat=.95, error='three', interval=100000, seed=0):
     stopat = float(stopat)                  # FAHID stop at recall
-    starting = 1                            # FAHID Do random sampling until
+    starting = 5                            # FAHID Do random sampling until
     np.random.seed(seed)
 
     read = MAR()
     read = read.create(filename)
 
-    read.interval = interval                # FAHID LATER TRAIN USE IT correct errors with human-machine disagreements
+    #read.BM25(query.strip().split('_'))     # FAHID TODO REMOVE
 
-    read.BM25(query.strip().split('_'))     # FAHID LATER
+    num_of_total_pos = read.get_allpos()
+    target = int(num_of_total_pos * stopat)
 
-    num2 = read.get_allpos()
-    target = int(num2 * stopat)
+    print ("Target: " + str(target))
 
     while True:
         pos, neg, total = read.get_numbers()
-        print("%d, %d" % (pos, pos + neg))
+        # print("%d, %d" % (pos, pos + neg))
+
 
         if pos + neg >= total:
             break
 
         if pos < starting:
-            # FAHID TODO LETS RANDOMLY PICK self.step undetermined values
-            for id in read.BM25_get():
+            for id in read.get_random_ids():
                 read.code_error(id, error=error)
         else:
             a, b, c, d = read.train(weighting=True, pne=True)
@@ -50,10 +58,27 @@ def active_learning(filename, query='', stopat=.95, error='none', interval=10000
                 # Certainity Sampling
                 for id in c:
                     read.code_error(id, error=error)
-    #set_trace()
-    return read
+
+    pos, neg, total = read.get_numbers()
+    print("Positive %d, Total Looked %d" % (pos, pos + neg))
+
+    checked_data = read.body.loc[read.body["code"]!='undetermined']
+    test = checked_data.loc[:, "label"].tolist()
+    predicted = checked_data.loc[:, "code"].tolist()
+    confusion_mat = confusion_matrix(test, predicted, labels=["no", "yes"])
+    score = metrics.classification_report(test, predicted, digits=3)
+
+    print(score)
+    print(confusion_mat)
+    # #set_trace()
+    # return read
 
 
 if __name__ == "__main__":
-    active_learning('Hall.csv')
+
+    active_learning('td_2016.csv')
+    print("END")
     # eval(cmd())
+
+
+    # TODO implement BM25
